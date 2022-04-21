10c8212e7fff52d1e9ef01cd8426caecd18c0bde
AlexandrGulyaev <imperator.alexandriv@yandex.ru>
2022-04-21 22:31:47 +0300

    Lab4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Image.delete_all
+Image.reset_pk_sequence
+Image.create([
+
+               {name: 'Постер к эпизоду 1 "Скрытая угроза"',     file: 'Poster_Episode_1.jpg', theme_id: 5},
+               {name: 'Постер к эпизоду 2 "Атака клонов"',       file: 'Poster_Episode_2.jpg', theme_id: 3},
+               {name: 'Постер к эпизоду 3 "Месть ситхов"',       file: 'Poster_Episode_3.jpg', theme_id: 4},
+               {name: 'Постер к эпизоду 4 "Новая надежда"',      file: 'Poster_Episode_4.jpg', theme_id: 2},
+               {name: 'Постер к эпизоду 5 "Империя наносит ответный удар"', file: 'Poster_Episode_5.jpg', theme_id: 3},
+               {name: 'Постер к эпизоду 6 "Возвращение джедая"', file: 'Poster_Episode_6.jpg', theme_id: 1},
+               {name: 'Постер к эпизоду 7 "Пробуждение силы"',   file: 'Poster_Episode_7.jpg', theme_id: 5},
+               {name: 'Постер к эпизоду 8 "Последний джедай"',   file: 'Poster_Episode_8.jpg', theme_id: 2},
+               {name: 'Постер к эпизоду 9 "Скайуокер. Восход"',  file: 'Poster_Episode_9.jpg', theme_id: 3},
+               {name: 'Постер к спин-оффу "Изгой-один"',         file: 'Poster_RogueOne.jpg',  theme_id: 4},
+               {name: 'Постер к спин-оффу "Соло"',               file: 'Poster_Solo.jpg',      theme_id: 5},
+             ])
+
+Theme.delete_all
+Theme.reset_pk_sequence
+Theme.create([
+
+               {name: "-----"},      # 1 Нет темы
+               {name: "Какой из эпизодов относится к оригинальной трилогии?"},      # 2
+               {name: "Какой из эпизодов не был срежессирован Джорджем Лукасом?"},  # 3
+               {name: "Композитором какого фильма не являлся Джон Уильямс?"},       # 4
+               {name: "Какой из эпизодов не был снят компанией Disney?"},           # 5
+             ])
+
+User.delete_all
+User.reset_pk_sequence
+User.create([
+
+              {name: "THRAWN", email: "thrawn@csilla.chiss"},
+            ])
+
