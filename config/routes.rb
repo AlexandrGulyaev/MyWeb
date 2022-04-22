7f06801ffec4d7d1bd25a4b1b9cc859e4b36bcb7
AlexandrGulyaev <imperator.alexandriv@yandex.ru>
2022-04-22 16:08:49 +0300

    Labs 4,5

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
   get 'main/help'
   get 'main/contacts'
   get 'main/about'
-  root 'main#index'
+  #root 'main#index'
+  root 'work#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # work
+
+  match 'work',             to: 'work#index',             via: 'get'
+  match 'choose_theme',     to: 'work#choose_theme',      via: :get
+  match 'display_theme',    to: 'work#display_theme',     via: :post
+
 end
